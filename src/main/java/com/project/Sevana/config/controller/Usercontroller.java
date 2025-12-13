626332e5c981dfaa605cca67978c5d1bf5414fed
albinpshaji <albinpshaji3@gmail.com>
2025-12-13 14:46:26 +0530

    added users files and connected with db , still not works as csrf is still enabled

---
+++ src/main/java/com/project/Sevana/config/controller/Usercontroller.java
@@ -0,0 +1,35 @@
+package com.project.Sevana.config.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.Sevana.model.Users;
+import com.project.Sevana.service.Userservice;
+
+@RestController
+@RequestMapping("/users")
+public class Usercontroller {
+	
+	private Userservice service;
+	
+	@Autowired
+	public Usercontroller(Userservice service) {
+		this.service=service;
+	}
+	@GetMapping("/")
+	public List<Users> getusers() {
+		return service.getusers();
+	}
+	
+	@PostMapping("/register")
+	public Users registeruser(@RequestBody Users user) {
+		return service.registeruser(user);
+	}
+	
+}
