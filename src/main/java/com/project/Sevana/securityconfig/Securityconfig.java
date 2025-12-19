e2ce1ad9b61c8a129256a048f217295a65a01de6
albinpshaji <albinpshaji3@gmail.com>
2025-12-20 00:23:33 +0530

    role-based access control and implement secure direct donation flow, a getmapping for distributors to check the incoming directdonations

--- src/main/java/com/project/Sevana/securityconfig/Securityconfig.java
+++ src/main/java/com/project/Sevana/securityconfig/Securityconfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,6 +26,7 @@ import com.project.Sevana.securityFilter.Jwtfilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity//this is to enable @PreAuthorize annotation so you can lock specific requests from specific users
 public class Securityconfig {
 
 	private final UserDetailsService userdetailsservice;
